51e9e394e6304c8dac62af3ded2a500ccdad549d
sathishn-coder <sathish.nethaji@bridgelabz.com>
2026-06-18 17:18:29 +0530

    modified

--- src/com/train/service/PassengerService.java
+++ src/com/train/service/PassengerService.java
@@ -1,5 +1,6 @@
 package com.train.service;
 
+import com.train.exception.InvalidCapacityException;
 import com.train.model.PassengerBogie;
 
 import java.util.ArrayList;
@@ -9,36 +10,38 @@ public class PassengerService {
 
     public List<PassengerBogie> getPassengerBogies() {
 
-        List<PassengerBogie> list =
-                new ArrayList<>();
+        List<PassengerBogie> list = new ArrayList<>();
 
-        list.add(
-                new PassengerBogie(
-                        "P101",
-                        "Coach-A",
-                        "Sleeper",
-                        72));
+        try {
 
-        list.add(
-                new PassengerBogie(
-                        "P102",
-                        "Coach-B",
-                        "Sleeper",
-                        80));
+            list.add(new PassengerBogie(
+                    "P101",
+                    "Coach-A",
+                    "Sleeper",
+                    72));
 
-        list.add(
-                new PassengerBogie(
-                        "P103",
-                        "Coach-C",
-                        "AC",
-                        60));
+            list.add(new PassengerBogie(
+                    "P102",
+                    "Coach-B",
+                    "Sleeper",
+                    80));
 
-        list.add(
-                new PassengerBogie(
-                        "P104",
-                        "Coach-D",
-                        "ChairCar",
-                        45));
+            list.add(new PassengerBogie(
+                    "P103",
+                    "Coach-C",
+                    "AC",
+                    60));
+
+            list.add(new PassengerBogie(
+                    "P104",
+                    "Coach-D",
+                    "ChairCar",
+                    45));
+
+        } catch (InvalidCapacityException e) {
+
+            System.out.println(e.getMessage());
+        }
 
         return list;
     }
